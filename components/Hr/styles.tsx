c186bfbe41ba4a7e08ec63321aedd25ac47e9dd7
Julien <julien.sebag@me.com>
2022-10-13 13:27:14 +0200

    Added margins hr

--- components/Hr/styles.tsx
+++ components/Hr/styles.tsx
@@ -4,19 +4,40 @@ import styled from "styled-components"
 
 import setDefaultTheme from "../../utils/setDefaultTheme"
 import { stringifyPx } from "ts-utils-julseb"
+import Mixins from "../../Mixins"
 
-import { AllColorsTypes } from "../../utils/common-types"
+import { AllColorsTypes, SpacersTypes } from "../../utils/common-types"
 
 const StyledHr = styled.hr<{
     $color?: AllColorsTypes
     $height?: number
     $width?: number | string
+    $margin?: number | SpacersTypes | "0 auto" | "auto"
+    $marginLeft?: number | SpacersTypes
+    $marginTop?: number | SpacersTypes
+    $marginRight?: number | SpacersTypes
+    $marginBottom?: number | SpacersTypes
 }>`
     width: ${({ $width }) => stringifyPx($width)};
     height: ${({ $height }) => $height}px;
     border: none;
     background-color: ${({ theme }) => theme.AllColors};
     display: block;
+    margin: ${({ $margin }) =>
+        $margin &&
+        ($margin === "0 auto"
+            ? "0 auto"
+            : $margin === "auto"
+            ? "auto"
+            : Mixins.Spacers({ $spacer: $margin }))};
+    margin-left: ${({ $marginLeft }) =>
+        $marginLeft && Mixins.Spacers({ $spacer: $marginLeft })};
+    margin-top: ${({ $marginTop }) =>
+        $marginTop && Mixins.Spacers({ $spacer: $marginTop })};
+    margin-right: ${({ $marginRight }) =>
+        $marginRight && Mixins.Spacers({ $spacer: $marginRight })};
+    margin-bottom: ${({ $marginBottom }) =>
+        $marginBottom && Mixins.Spacers({ $spacer: $marginBottom })};
 `
 
 setDefaultTheme([StyledHr])
--- components/Hr/types.tsx
+++ components/Hr/types.tsx
@@ -2,10 +2,21 @@
 
 import React from "react"
 
-import { AllColorsTypes } from "../../utils/common-types"
+import { AllColorsTypes, SpacersTypes } from "../../utils/common-types"
 
 export interface HrProps extends React.HTMLAttributes<HTMLHRElement> {
     color?: AllColorsTypes
     height?: number
     width?: number | string
+    margin?:
+        | number
+        | SpacersTypes
+        | "0 auto"
+        | "auto"
+        | {
+              left?: number | SpacersTypes
+              top?: number | SpacersTypes
+              right?: number | SpacersTypes
+              bottom?: number | SpacersTypes
+          }
 }
