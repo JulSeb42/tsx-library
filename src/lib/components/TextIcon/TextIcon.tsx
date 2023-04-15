52729ccfec4a17907f31b4d4f20abb74b6ffbb33
JulSeb42 <julien.sebag@me.com>
2023-04-15 15:12:06 +0200

    Clean TextIcon

--- src/lib/components/TextIcon/TextIcon.tsx
+++ src/lib/components/TextIcon/TextIcon.tsx
@@ -4,10 +4,9 @@ import React, { forwardRef } from "react"
 import type { ForwardedRef } from "react"
 
 import { Icon, Text } from "../../"
-import { getIconSize } from "./get-icon-size"
 
 import * as Styles from "./styles"
-import type { TextIconProps } from "./types"
+import type { TextIconProps, PossibleTextIconTags } from "./types"
 
 const TextIcon = forwardRef(
     (
@@ -51,5 +50,49 @@ const TextIcon = forwardRef(
         )
     }
 )
+
+const getIconSize = (
+    tag: PossibleTextIconTags,
+    display: boolean | undefined,
+    iconSize?: number
+) => {
+    if (iconSize) return iconSize
+
+    if (display) {
+        switch (tag) {
+            case "h1":
+                return 80
+            case "h2":
+                return 64
+            case "h3":
+                return 56
+            case "h4":
+                return 48
+            case "h5":
+                return 40
+        }
+    }
+
+    switch (tag) {
+        case "h1":
+            return 40
+        case "h2":
+            return 32
+        case "h3":
+            return 28.8
+        case "h4":
+            return 24
+        case "h5":
+            return 20.8
+        case "h6":
+        case "blockquote":
+            return 17.6
+        case "small":
+            return 14
+        case "p":
+        default:
+            return 16
+    }
+}
 
 export default TextIcon
